a79453c22630a1ae34e72111cece5f7cd424368e
Geoff Thorpe <geoffrey@twosigma.com>
2021-09-14 16:26:51 -0400

    hcp: fix regression due to asset-signing
    Signed-off-by: Geoff Thorpe <geoffrey@twosigma.com>

--- hcp/enrollsvc/run_mgmt.sh
+++ hcp/enrollsvc/run_mgmt.sh
@@ -4,6 +4,10 @@
 
 expect_root
 
+echo "Chowning asset-signing keys for use by db_user"
+
+chown db_user:db_user $SIGNING_KEY_PRIV $SIGNING_KEY_PUB
+
 echo "Running 'enrollsvc-mgmt' service"
 
 drop_privs_flask /hcp/enrollsvc/flask_wrapper.sh
